cc5cdcb66b927077586ae5d16975c17414a62b69
Nilesh Kiran Sonawane <sonawanenileshk6@gmail.com>
2025-12-21 17:25:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// export const host = "0.0.0.0"; // Required for Render/Heroku/etc.
+export const port = process.env.PORT || 8080; // Render gives port via env
+
+// export const host = "localhost";
+// export const port =  8080;
---
+++ server.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+const config = require('./config');
+
+// Create WebSocket server
+const wss = new WebSocket.Server({ port: config.port});
+
+// Store overall match log
+let scoreLog = [];
+
+// Broadcast to all clients
+function broadcast(data) {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+wss.on('connection', (ws) => {
+  console.log("✅ New client connected");
+
+  // // Send full score log on connection
+  // ws.send(JSON.stringify({ type: "fullLog", log: scoreLog }));
+
+  ws.on('message', (message) => {
+    try {
+      let data = JSON.parse(message);
+      console.log("📢 Received:", data);
+      if (data.for == 'Cricketpanel') {
+        // Broadcast updated log
+        broadcast({ type: data.for, log: data.score });
+        console.log("📢 Update:", entry);
+      }
+    } catch (err) {
+      console.error("❌ Invalid message:", message);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log("❌ Client disconnected");
+  });
+});
+
+console.log(`🚀 LiveStrike WebSocket Server running on ws://${config.port}`);
